57a594faaaca93a8a39c16d039dd9b16e4dbd6f6
Corey Montella <cmontella@live.com>
2018-01-05 17:17:33 -0800

    Print a character to the screen with a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,16 @@
 #![feature(lang_items)]
+#![feature(unique)]
+#![feature(const_fn)]
 #![no_std]
+
 extern crate rlibc;
 
 mod vga_buffer;
 
 #[no_mangle]
 pub extern fn hivemind_entry() {
-    let hello = b"Hivemind";
-    let color_byte = 0x1f;
 
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
+    vga_buffer::print_something();
     loop{}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 #[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Color {
     Black       =   0,
@@ -19,10 +20,85 @@ pub enum Color {
     White       =   15,
 }
 
+// A Color code consists of an 8 bit foreground and background color.
+
+#[derive(Debug, Clone, Copy)]
 struct ColorCode(u8);
 
 impl ColorCode {
     const fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
+}
+
+// We represent the screen as a matrix of characters 80 wide by 25 height
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)] // Lay out struct as in C for correct field ordering.
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+// Create a writer type that writes to the screen.
+
+use core::ptr::Unique;
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: Unique<Buffer>,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            // Move to a new line on a new line char
+            b'\n' => self.new_line(),
+            // Place a char in the screen buffer on for any other byte
+            byte => {
+                // Insert a new line if we've reached the end of the screen
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+
+                // Place the character into the buffer at the position (row,col)
+                self.buffer().chars[row][col] = ScreenChar {
+                  ascii_character: byte,
+                  color_code: color_code,  
+                };
+
+                self.column_position += 1;
+            }
+        }
+    }
+
+    // Converts raw pointer to a safe buffer reference
+    fn buffer(&mut self) -> &mut Buffer {
+        unsafe{ self.buffer.as_mut() }
+    }
+
+    fn new_line(&mut self) { 
+        /* TODO */
+
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightBlue, Color::Black),
+        buffer: unsafe { Unique::new_unchecked(0xb8000 as *mut _) },
+    };
+    writer.write_byte(b'Q');
 }
