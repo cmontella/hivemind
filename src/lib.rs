6a8f931e83982e12463f64c2382aae6d75153f2b
Corey Montella <cmontella@live.com>
2018-01-05 20:41:18 -0800

    Print memory map info

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,25 @@
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn hivemind_entry() {
-
+pub extern fn hivemind_entry(multiboot_info_address: usize) {
+    // Start by clearing the screen
     vga_buffer::clear_screen();
-    println!("Hello World");
-    println!("Hello Hivemind");
-    println!("{}", { println!("inner"); "outer" });
+
+    // Get mapped memory areas
+    let boot_info = unsafe { multiboot2::load(multiboot_info_address)};
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required.");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
+
     loop{}
 }
 
