e9bddb1cfad90f3718bb29f1a9b7029ad91b603d
Corey Montella <cmontella@live.com>
2018-01-06 13:12:30 -0800

    Test the memory module

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
+mod memory;
+
+use memory::FrameAllocator;
 
 #[no_mangle]
 pub extern fn hivemind_entry(multiboot_info_address: usize) {
@@ -40,6 +43,16 @@ pub extern fn hivemind_entry(multiboot_info_address: usize) {
     for section in elf_sections_tag.sections() {
         println!("    address: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section.addr, section.size, section.flags);
     }
+
+    /* ------- Test Memory Allocation ------- */
+    let mut frame_allocator = memory::AreaFrameAllocator::new(kernel_start as usize, kernel_end as usize,
+                                                              multiboot_start, multiboot_end, memory_map_tag.memory_areas());
+    for i in 0..3000 {
+        frame_allocator.allocate_frame();
+    }
+
+    println!("Boot complete");
+
 
     loop{}
 }
