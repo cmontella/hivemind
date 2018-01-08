08d44e1194aa03ab5aca2f2c380a14e3c6fedfeb
Corey Montella <cmontella@live.com>
2018-01-07 17:56:39 -0800

    fix boot loop

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -28,10 +28,15 @@ start:
 ; Link P4 first entry to the P3 table, and the P3 first entry to the P2 table
 
 set_up_page_tables:
+    ; map P4 table recursively
+    mov eax, p4_table
+    or eax, 0b11 ; present + writable
+    mov [p4_table + 511 * 8], eax
+
     ; map first P4 entry to P3 table
     mov eax, p3_table
     or eax, 0b11 ; present + writable
-    mov [p4_table + 511 * 8], eax
+    mov [p4_table], eax
 
     ; map first P3 entry to P2 table
     mov eax, p2_table
