f9478135fc92592b0c110a9b217f11658851e798
cmontella <cmontella@live.com>
2018-01-21 23:12:01 -0800

    Stub in handling breakpoints

--- src/memory/interrupts.rs
+++ src/memory/interrupts.rs
@@ -1,5 +1,12 @@
-use x86_64::structures::idt::Idt;
+use x86_64::structures::idt::{Idt, ExceptionStackFrame};
 
 pub fn init() {
   let mut idt = Idt::new();
+  idt.breakpoint.set_handler_fn(breakpoint_handler);
+}
+
+// Handle breakpoints
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
+  println!("A breakpoint exception occurred\n{:#?}", stack_frame);
 }
