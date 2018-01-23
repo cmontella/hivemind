ec8fd1ed09f6c87c0020e36c753db284865703e1
cmontella <cmontella@live.com>
2018-01-22 19:20:05 -0800

    Put it all together. This should fix triple faults

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -2,12 +2,16 @@ use x86_64::structures::idt::{Idt, ExceptionStackFrame};
 use memory::MemoryController;
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtualAddress;
+use spin::Once;
 
 mod gdt;
 
 // The zeroth IST entry is the double fault stack. Any other one would work,
 // but this is fine.
 const DOUBLE_FAULT_IST_INDEX: usize = 0;
+
+static TSS: Once<TaskStateSegment> = Once::new();
+static GDT: Once<gdt::Gdt> = Once::new();
 
 // Interrupt Descriptor Table (IDT)
 // The IDT hold pointers to handler functions for various exceptions and interrupts.
@@ -16,7 +20,10 @@ lazy_static! {
     static ref IDT: Idt = {
         let mut idt = Idt::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(DOUBLE_FAULT_IST_INDEX as u16);
+        }
         idt
     };
 }
@@ -24,17 +31,42 @@ lazy_static! {
 // Initialize the IDT
 
 pub fn init(memory_controller: &mut MemoryController) {
+    use x86_64::structures::gdt::SegmentSelector;
+    use x86_64::instructions::segmentation::set_cs;
+    use x86_64::instructions::tables::load_tss;
+
     // We allocate one page (4096 bytes) for our double fault handler.
     let handler_pages = 1; 
     let double_fault_stack = memory_controller.alloc_stack(handler_pages)
-        .expect("Could not allocate double fault stack");
+        .expect("Could not allocate double fault stack.");
 
     // Create a Task State Segment (TSS) that contains our double fault stack
     // in its interrupt stack table.
+    let tss = TSS.call_once(|| {
+        let mut tss = TaskStateSegment::new();
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX] = VirtualAddress(
+            double_fault_stack.top()); // Load to top and it grows down.
+        tss
+    });
 
-    let mut tss = TaskStateSegment::new();
-    tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX] = VirtualAddress(
-        double_fault_stack.top()); // Load to top and it grows down.
+    // Load TSS into GDT
+    let mut code_selector = SegmentSelector(0);
+    let mut tss_selector = SegmentSelector(0);
+    let gdt = GDT.call_once(|| {
+        let mut gdt = gdt::Gdt::new();
+        code_selector = gdt.add_entry(gdt::Descriptor::kernel_code_segment());
+        tss_selector = gdt.add_entry(gdt::Descriptor::tss_segment(&tss));
+        gdt
+    });
+    gdt.load();
+
+
+    unsafe {
+        // Reload code segment register
+        set_cs(code_selector);
+        // Load TSS
+        load_tss(tss_selector);
+    }
 
     IDT.load();
 }
