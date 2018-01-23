0cb1c2d8ac066ff88e29f510d15d348530c45059
cmontella <cmontella@live.com>
2018-01-22 18:08:02 -0800

    Handle a class of double faults

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,6 +4,7 @@ lazy_static! {
     static ref IDT: Idt = {
         let mut idt = Idt::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -16,4 +17,36 @@ pub fn init() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
   println!("A breakpoint exception occurred\n{:#?}", stack_frame);
+}
+
+// Handle double faults. Double faults can only occur in specific combinations of exceptions
+
+/*
+In the case of:
+Divide-by-zero,
+Invalid TSS,
+Segment Not Present,
+Stack-Segment Fault,
+General Protection Fault	
+
+A Double double fault occurs if:
+Invalid TSS,
+Segment Not Present,
+Stack-Segment Fault,
+General Protection Fault
+
+In the case of:
+Page Fault	
+
+A Double double fault occurs if:
+Page Fault,
+Invalid TSS,
+Segment Not Present,
+Stack-Segment Fault,
+General Protection Fault
+*/
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackFrame, _error_code: u64) {
+    println!("\nEXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    loop {}
 }
