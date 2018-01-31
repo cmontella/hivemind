2b71669182bdd6268cdbe3824ab05272ac180a11
cmontella <cmontella@live.com>
2018-01-31 15:59:54 -0800

    extend transaction implementaitons

--- src/database/transaction.rs
+++ src/database/transaction.rs
@@ -2,13 +2,15 @@
 
 use alloc::{String, Vec};
 use interrupts::event;
-
+use core::fmt;
 
 /*
 Transactions are units of atomic updates to the DB.
 */
 
-#[derive(Debug)]
+
+// ## Values
+#[derive(Clone)]
 pub enum Value {
   Null,
   Number(u64),
@@ -32,13 +34,26 @@ impl Value {
 
 }
 
+impl fmt::Debug for Value {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match self {
+        &Value::Number(ref x) => write!(f, "{:?}", x),
+        &Value::String(ref x) => write!(f, "{:?}", x),
+        &Value::Bool(ref x) => write!(f, "{:?}", x),
+        &Value::Null => write!(f, "Null"),
+      }
+    }
+}
+
+// ## Changes
+
 #[derive(Debug)]
 pub enum ChangeType {
   Add,
   Remove,
 }
 
-#[derive(Debug)]
 pub struct Change {
   pub kind: ChangeType,
   pub entity: String,
@@ -81,23 +96,48 @@ impl Change {
   }
 }
 
-#[derive(Debug)]
+impl fmt::Debug for Change {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}: [{:?} {:?}: {:?}]", self.kind, self.entity, self.attribute, self.value)
+    }
+}
+
+
+// ## Transactions
+
 pub struct Transaction {
-  timestamp: u64,
-  complete: bool,
+  pub timestamp: u64,
+  pub complete: u64,
   pub adds: Vec<Change>,
   pub removes: Vec<Change>,
-
 }
 
 impl Transaction {
   pub fn new() -> Transaction {
     Transaction {
       timestamp: event::update_time(),
-      complete: false,
+      complete: 0,
       adds: Vec::new(),
       removes: Vec::new(),
     }
   }
+
+  pub fn process(&mut self) -> u64 {
+    if self.complete == 0 {
+      self.complete = event::update_time();
+    }
+    self.complete
+  }
+
+  pub fn is_complete(&self) -> bool {
+    self.complete != 0
+  }
 }
 
+impl fmt::Debug for Transaction {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}: Add: {:?}  Remove: {:?}", self.timestamp, self.adds.len(), self.removes.len())
+    }
+}
