1abc945830dc7c3471b7382a449457bcccdb96db
Corey Montella <cmontella@live.com>
2019-05-08 19:19:11 -0400

    implement a screen writer and println

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 #![no_std]
 #![no_main]
 
+extern crate lazy_static;
+extern crate spin;
+extern crate volatile;
+
 use core::panic::PanicInfo;
+use core::fmt::Write;
+
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
