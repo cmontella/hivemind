051c0e5c8d3024edfe97c8eafa5cd1494ace10ff
Corey Montella <cmontella@live.com>
2019-07-25 18:53:47 -0400

    update main

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,32 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
-#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
-#![feature(allocator_api)]
+#![feature(abi_x86_interrupt)]
 #![feature(alloc_error_handler)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate lazy_static;
-extern crate spin;
-extern crate volatile;
-extern crate x86_64;
-extern crate uart_16550;
-extern crate pic8259_simple;
-extern crate pc_keyboard;
-extern crate bootloader;
 extern crate alloc;
-extern crate linked_list_allocator;
-extern crate mech_core;
 
 use core::panic::PanicInfo;
-use core::fmt::Write;
-use x86_64::instructions::port::Port;
-#[cfg(test)]
-use bootloader::{entry_point, BootInfo};
 use linked_list_allocator::LockedHeap;
-use mech_core::Core;
-use lazy_static::lazy_static;
-use spin::Mutex;
 
+pub mod allocator;
+pub mod gdt;
+pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
-pub mod interrupts;
-pub mod gdt;
-pub mod memory;
-pub mod allocator;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
@@ -51,6 +43,30 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 #[cfg(test)]
 entry_point!(test_kernel_main);
@@ -69,41 +85,7 @@ fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub fn hlt_loop() -> ! {
-    loop {
-        x86_64::instructions::hlt();
-    }
-}
-
-lazy_static! {
-  pub static ref Mech: Mutex<Core> = Mutex::new(Core::new(1000, 10));
-}
-
-
-pub fn init() {
-    gdt::init();
-    interrupts::init_idt();
-    unsafe { interrupts::PICS.lock().initialize() };
-    x86_64::instructions::interrupts::enable();
-}
-
-
-pub const HEAP_START: usize = 0o_000_001_000_000_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
-
-#[global_allocator]
-static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,18 @@
 #![test_runner(hivemind::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate hivemind;
-extern crate bootloader;
-extern crate x86_64;
-#[macro_use]
 extern crate alloc;
 
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use hivemind::println;
-use hivemind::memory::{self, BootInfoFrameAllocator};
-use core::panic::PanicInfo;
 use bootloader::{entry_point, BootInfo};
-use alloc::boxed::Box;
-use alloc::vec::Vec;
-use x86_64::{structures::paging::Page, VirtAddr};
+use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use hivemind::allocator;
+    use hivemind::memory::{self, BootInfoFrameAllocator};
 
     println!("Hello World{}", "!");
     hivemind::init();
@@ -28,13 +23,31 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // map a previously unmapped page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
