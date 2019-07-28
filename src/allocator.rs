48c54197d4f97ebf0b386fe9d592ef1b064a25f7
Corey Montella <cmontella@live.com>
2019-07-28 19:02:34 -0400

    increase heap size

--- src/allocator.rs
+++ src/allocator.rs
@@ -8,7 +8,7 @@ use x86_64::{
 };
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 300 * 1024; // 100 KiB
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
